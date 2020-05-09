60d89d5a3d43a7626c94e54f2bef2b7a34ac2ccd
mehrdad jalebi <mehrdad.jalebi@gmail.com>
2020-05-09 21:07:52 +0430

    chore: hair - left part - base

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,21 @@
 </script>
 
 <main>
-  <div class="face">
-  </div>
+<div class="mainImage">
+	<div class="head">
+		<div class="hair">
+			<div class="center-part"></div>
+			<div class="left-part"></div>
+			<div class="left-middle-part"></div>
+			<div class="left-down-part"></div>
+		</div>
+		<div class="eyebrows"></div>
+		<div class="eyes"></div>
+		<div class="nose"></div>
+		<div class="lips"></div>
+		<div class="neck"></div>
+	</div>
+</div>
 </main>
 
 <style type="text/scss">
